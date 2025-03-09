4d94e44b1ed04e86356f88293548e1b077974651
mounettemounder <mounettemounder@gmail.com>
2025-03-09 02:28:06 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My School Project</h1>
+    <p>This is a comprehensive school project that covers various topics and skills.</p>
+  `,
+})
+export class AppComponent { }
