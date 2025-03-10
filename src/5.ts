af8ad4563b688fc4a0154eb514db19b9666d6535
mounettemounder <mounettemounder@gmail.com>
2025-03-10 10:22:19 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((response: any) => {
+      this.heroes = response;
+    });
+  }
+}
