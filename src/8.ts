9be5b5a84c4b8d449f41379338b337af1de09b61
mounettemounder <mounettemounder@gmail.com>
2025-03-13 11:19:57 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-school-project',
+  templateUrl: './my-school-project.component.html',
+  styleUrls: ['./my-school-project.component.css']
+})
+export class MySchoolProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
