1ae7f1a67ac134c1a4d0621c398b487fe40f022d
mounettemounder <mounettemounder@gmail.com>
2025-03-18 22:06:52 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,11 @@
+// MySchoolProject.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-school-project',
+  templateUrl: './my-school-project.component.html',
+  styleUrls: ['./my-school-project.component.css']
+})
+export class MySchoolProjectComponent {
+  constructor() {}
+}
