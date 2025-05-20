c5888c437c629f1ab5c17de63b3fd5fcb9004052
mounettemounder <mounettemounder@gmail.com>
2025-05-20 10:46:04 +0000

    Add 41.ts

---
+++ src/41.ts
@@ -0,0 +1,9 @@
+class ExampleClass:
+    def __init__(self):
+        self.some_attribute = "some_value"
+
+    def some_method(self):
+        print(f"Some method called with {self.some_attribute}.")
+
+example_class = ExampleClass()
+example_class.some_method()
